1cae1d31edeb440399b93f228f9856cd7a54d76d
sayushin <shinya.sayu@gmail.com>
2022-05-08 09:12:21 +0900

    Layout,Header and footer

--- pages/events/add.js
+++ pages/events/add.js
@@ -1,9 +1,11 @@
+import Layout from "../../components/Layout"
+
 export default function AddEventPage() {
   return (
-    <div>
+    <Layout title='Add New Event'>
     <h1>
     Add Event
     </h1>
-    </div>
+    </Layout>
   )
 }
